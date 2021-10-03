dae20521cde473ac9d70a68bc8986ee358661c19
SivaNagendra-sn <sivanagendra.paruchuri@gmail.com>
2021-10-03 09:59:58 +0530

    Models code organized

--- and.py
+++ and.py
@@ -3,24 +3,33 @@ from utils.all_utils import prepare_data, save_model, save_plot
 from utils.model import Perceptron
 import pandas as pd
 
-AND = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,0,0,1],
-}
+def main(data , eta, epochs, filename, plot_file_name):
+    
 
-df = pd.DataFrame(AND)
+    df = pd.DataFrame(AND)
 
-X,y = prepare_data(df)
+    X,y = prepare_data(df)
 
-ETA = 0.3 # 0 and 1
-EPOCHS = 10
+    ETA = eta # 0 and 1
+    EPOCHS = epochs
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
+    model = Perceptron(eta=ETA, epochs=EPOCHS)
+    model.fit(X, y)
 
-_ = model.total_loss()
+    _ = model.total_loss()
 
 
-save_model(model, "and.model")
-save_plot(df, "and.png", model)
+    save_model(model, filename)
+    save_plot(df, plot_file_name, model)
+
+if __name__ == '__main__':
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    ETA = 0.3
+    Epochs = 10
+    file_name = "and.model"
+    plot_file_name = "and.png"
+    main(data=AND, eta=ETA, epochs=Epochs, filename= file_name, plot_file_name= plot_file_name)
---
+++ or.py
@@ -0,0 +1,35 @@
+from utils.all_utils import prepare_data, save_model, save_plot
+
+from utils.model import Perceptron
+import pandas as pd
+
+def main(data , eta, epochs, filename, plot_file_name):
+    
+
+    df = pd.DataFrame(AND)
+
+    X,y = prepare_data(df)
+
+    ETA = eta # 0 and 1
+    EPOCHS = epochs
+
+    model = Perceptron(eta=ETA, epochs=EPOCHS)
+    model.fit(X, y)
+
+    _ = model.total_loss()
+
+
+    save_model(model, filename)
+    save_plot(df, plot_file_name, model)
+
+if __name__ == '__main__':
+    OR = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,1,1,1],
+    }
+    ETA = 0.3
+    Epochs = 10
+    file_name = "or.model"
+    plot_file_name = "or.png"
+    main(data=OR, eta=ETA, epochs=Epochs, filename= file_name, plot_file_name= plot_file_name)
